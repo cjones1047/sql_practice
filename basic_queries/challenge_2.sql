2c4a2d1153fc415d8b0ecea567f97613b2dfbc46
cjones1047 <cjmj1047@gmail.com>
2022-10-05 18:00:10 -0500

    Completed challenge where queries intended results were 1) Create a list of all the distinct districts 2) Find the latest rental date 3) Find how many films the company has 4) Find the number of distinct last names of customers

---
+++ basic_queries/challenge_2.sql
@@ -0,0 +1,18 @@
+-- SELECT DISTINCT
+-- district
+-- FROM address
+
+-- SELECT
+-- rental_date
+-- FROM rental
+-- ORDER BY rental_date DESC
+-- LIMIT 1
+
+-- SELECT COUNT(film_id)
+-- FROM film
+
+SELECT DISTINCT COUNT(last_name) AS "Distinct Customer Last Names"
+FROM customer
+
+
+
