73a5f407257693371f08910f7cfc275031f9181c
cjones1047 <cjmj1047@gmail.com>
2022-10-06 05:47:38 -0500

    Completed challenge where queries intended results were 1) Find how many payments were made by the customer with customer_id equal to 100 2) Find the first name of the customer with the last name 'ERICA'

---
+++ basic_queries/challenge_3.sql
@@ -0,0 +1,9 @@
+-- SELECT customer_id, COUNT(*) AS "Number of Payments"
+-- FROM payment
+-- WHERE customer_id = 100
+-- GROUP BY customer_id
+
+SELECT last_name, first_name
+FROM customer
+WHERE first_name = 'ERICA'
+
