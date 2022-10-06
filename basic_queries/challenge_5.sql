db5c798e5d58a6ea7f46a3c30b09ecf70830b7cf
cjones1047 <cjmj1047@gmail.com>
2022-10-06 06:23:58 -0500

    Completed challenge where queries intended results were all payments where customer_id was one of three values and amounts were less than 2 dollars or greater than 10 dollars

---
+++ basic_queries/challenge_5.sql
@@ -0,0 +1,7 @@
+SELECT customer_id, amount
+FROM payment
+WHERE (customer_id = 322 OR
+customer_id = 346 OR
+customer_id = 354) AND
+(amount < 2 OR amount > 10)
+ORDER BY customer_id ASC, amount DESC
