61ae9226e98d4d8a44a482723dfc70d6ba84af0c
cjones1047 <cjmj1047@gmail.com>
2022-10-12 13:37:20 -0500

    Challenge 9 lists average sales for each staff member per customer

---
+++ project_1/challenge_9.sql
@@ -0,0 +1,8 @@
+SELECT 
+staff_id,
+ROUND(AVG(total),2) as avg_amount 
+FROM (
+SELECT SUM(amount) as total,customer_id,staff_id
+FROM payment
+GROUP BY customer_id, staff_id) AS a
+GROUP BY staff_id
