44435a85028ef4f7370fe82e02cccffbcc093050
cjones1047 <cjmj1047@gmail.com>
2022-10-14 08:46:33 -0500

    Challenge 13 lists all payments with their payment id, amount, category name, and total amounts per category ordered by category name ascending

---
+++ project_1/challenge_13.sql
@@ -0,0 +1,29 @@
+SELECT
+title,
+amount,
+name,
+payment_id,
+(SELECT SUM(amount) FROM payment p
+LEFT JOIN rental r
+ON r.rental_id=p.rental_id
+LEFT JOIN inventory i
+ON i.inventory_id=r.inventory_id
+LEFT JOIN film f
+ON f.film_id=i.film_id
+LEFT JOIN film_category fc
+ON fc.film_id=f.film_id
+LEFT JOIN category c1
+ON c1.category_id=fc.category_id
+WHERE c1.name=c.name)
+FROM payment p
+LEFT JOIN rental r
+ON r.rental_id=p.rental_id
+LEFT JOIN inventory i
+ON i.inventory_id=r.inventory_id
+LEFT JOIN film f
+ON f.film_id=i.film_id
+LEFT JOIN film_category fc
+ON fc.film_id=f.film_id
+LEFT JOIN category c
+ON c.category_id=fc.category_id
+ORDER BY name
