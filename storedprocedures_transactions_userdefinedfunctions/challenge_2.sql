31642d93418b5fefaed3454b3d0048ac8b2a45e7
cjones1047 <cjmj1047@gmail.com>
2022-10-17 09:49:52 -0500

    Challenge 2 swaps the position and salary data from one employee to another employee

---
+++ storedprocedures_transactions_userdefinedfunctions/challenge_2.sql
@@ -0,0 +1,22 @@
+SELECT *
+FROM employees
+WHERE first_name IN('Miller', 'Christalle') AND last_name IN('McQuarter', 'McKenny');
+
+BEGIN;
+UPDATE employees
+SET salary = 12587.00
+WHERE emp_id = 2;
+UPDATE employees
+SET position_title = 'Head of Sales'
+WHERE emp_id = 2;
+UPDATE employees
+SET salary = 14614.00
+WHERE emp_id = 3;
+UPDATE employees
+SET position_title = 'Head of BI'
+WHERE emp_id = 3;
+COMMIT;
+
+SELECT *
+FROM employees
+WHERE first_name IN('Miller', 'Christalle') AND last_name IN('McQuarter', 'McKenny');
