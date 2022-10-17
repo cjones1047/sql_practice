c6e2c5a1bec09e6f91883841ce270d89b5d58e9c
cjones1047 <cjmj1047@gmail.com>
2022-10-17 07:29:43 -0500

    Task 2 performs five different alterations to employees table

---
+++ project_2/task_2.sql
@@ -0,0 +1,8 @@
+ALTER TABLE employees
+ALTER COLUMN department_id SET NOT NULL,
+ALTER COLUMN start_date SET DEFAULT CURRENT_DATE,
+ADD COLUMN end_date DATE,
+ADD CONSTRAINT birth_check CHECK(birth_date < CURRENT_DATE);
+
+ALTER TABLE employees
+RENAME COLUMN job_position TO position_title;
